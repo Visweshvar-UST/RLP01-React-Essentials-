1e99aaa5c8b5ab98375b6467816208c729309129
Visweshvar-UST <visweshvar.ust@gmail.com>
2024-12-11 15:17:13 +0530

    shortform

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept {...CORE_CONCEPTS[0]}/> //short form by same names
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
           </ul>
